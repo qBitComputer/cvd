ae52607e996c0c99ca8b8308fe2aaa35ecc96dbc
Job <jobmeijer@hotmail.com>
2024-03-28 23:49:11 +0100

    *servomotor assignment afgemaakt

--- motor_encoder/motor_encoder/motor_encoder.ino
+++ motor_encoder/motor_encoder/motor_encoder.ino
@@ -10,25 +10,24 @@
 #define PIN_ENC2 3
 #define PIN_MOT1 5
 #define PIN_MOT2 6
-
 #define SAMPLE_US 10000   // 10ms per iteratie van de regellus, dus 100Hz
 
 // Encoder variabelen
-volatile long encoderCount = 0;
-volatile boolean aSet = false;
-volatile boolean bSet = false;
+long encoderCount = 0;
+boolean aSet = false;
+boolean bSet = false;
 
 // PID controller constanten
-double Kp = 8.0;    // Proportional gain
-double Ki = 2.0;    // Integral gain
-double Kd = 0.1;    // Derivative gain
+double Kp = 20.0;                                   // Proportional gain
+double Ki = 0.0;                                    // Integral gain
+double Kd = 0.3;                                    // Derivative gain
+double sampleTimeS = (double)SAMPLE_US / 1000000;   // Converteren sample tijd naar secondes
 
-// PID variables
-double setPoint = 100.0;    // Gewenste positie
-double input = 0;           // 
+// PID variabelen
+double setpoint = 100;      // Gewenste positie
 double output = 0;          // Output van PID controller, dus input naar PWM signaal welke naar de motor controller gaat
 double error = 0;           // Berekende fout (verschil tussen gemeten waarde en setpoint)
-double previousError = 0;   // Fout tijdens vorige berekening
+double previousError = 0;   // Fout van vorige berekening, nodig voor berekenen derative
 double integral = 0;        // Om waarde integraal op te slaan  
 double derivative = 0;      // Om waarde differentiaal op te slaan
 unsigned long lastTime = 0; // Laatste tijdstip waarop PID is berekend
@@ -58,14 +57,10 @@ void loop() {
     String data = Serial.readStringUntil('\n');
 
     // Converteer de string naar integer zodat we 'm kunnen gebruiken voor de PID controller
-    int newSetPoint = data.toInt();
+    int newSetpoint = data.toInt();
 
-    // Check of de conversie gelukt is en er geen onzin uit komt
-    if (newSetPoint != 0 || data == "0") {
-      setPoint = newSetPoint;
-      Serial.print("New setpoint: ");
-      Serial.println(setPoint);
-    }
+    // Check of de conversie gelukt is en er geen onzin uit komt, zet hem daarna door als setpoint
+    if (newSetpoint != 0 || data == "0") setpoint = newSetpoint;      
   }
 }
 
@@ -78,11 +73,8 @@ void updateEncoder() {
   aSet = digitalRead(PIN_ENC1);
   bSet = digitalRead(PIN_ENC2);
 
-  if (aSet != bSet) {
-    encoderCount++;
-  } else {
-    encoderCount--;
-  }
+  if (aSet != bSet) encoderCount++;
+  else encoderCount--;
 }
 
 // Elke keer als timer1 een signaal geeft worden de PID waardes berekend en naar de motor controller gestuurd
@@ -91,31 +83,24 @@ void calculatePID() {
   Serial.print(0);    // Prachtige manier om serial plotter y-as schaling mooi te houden
   Serial.print('\t');
   Serial.println(encoderCount);
-
-  // input van de PID controller is de gemeten encoder positie
-  input = encoderCount; 
-  unsigned long currentTime = millis();
-  double elapsedTime = (double)(currentTime - lastTime) / 1000; // Convert to seconds
-
-  error = setPoint - input;                             // Berekenen error
-  integral += error * elapsedTime;                      // Berekenen integral
-  derivative = (error - previousError) / elapsedTime;   // Berekenen derivative
-
-  // Bereken PID output
-  output = (Kp * error) + (Ki * integral) + (Kd * derivative);
+  
+  // Berekeningen PID controller
+  error = setpoint - encoderCount;                              // Berekenen error
+  integral += error * sampleTimeS;                              // Berekenen integral
+  derivative = (error - previousError) / sampleTimeS;           // Berekenen derivative  
+  output = (Kp * error) + (Ki * integral) + (Kd * derivative);  // Bereken PID output
 
   // Draairichting van motor veranderen als output negatief wordt
   if (output > 0) {
     digitalWrite(PIN_MOT1, HIGH);
     digitalWrite(PIN_MOT2, LOW);
-    analogWrite(PIN_MOT1, min(abs(output), 255)); // Ensure output doesn't exceed PWM limits
+    analogWrite(PIN_MOT1, min(abs(output), 255)); // Beveiligen dat de output hoger wordt dan de maximale waarde van 255
   } else {
     digitalWrite(PIN_MOT1, LOW);
     digitalWrite(PIN_MOT2, HIGH);
-    analogWrite(PIN_MOT2, min(abs(output), 255)); // Ensure output doesn't exceed PWM limits
+    analogWrite(PIN_MOT2, min(abs(output), 255)); 
   }
 
   // Opslaan error en tijd voor volgende berekening
   previousError = error;
-  lastTime = currentTime;
 }
